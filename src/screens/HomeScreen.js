062ca4b28c831c5d28d01cc808b289205663ba58
Neba Emmanuel <nebaemmanuel736@gmail.com>
2023-02-24 22:42:05 +0100

    navigation_added

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,14 +1,32 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, View, Button, TouchableOpacity} from "react-native";
 
-const HomeScreen = () => {
-  return <Text style={styles.text}>Hello There!</Text>;
+const HomeScreen = ({navigation}) => {
+  return (
+    <View>
+      <Text style={styles.text}>Hello There!</Text>
+      <Button 
+      onPress={() => navigation.navigate('Components')}
+      title="Go to component demo" 
+      style={styles.buttonStyle}/>
+
+      <TouchableOpacity onPress={() => navigation.navigate('fList')}>
+        <Text>Go to List demo</Text>
+      </TouchableOpacity>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
   text: {
     fontSize: 30,
   },
+
+  buttonStyle:{
+    width: 50,
+    height: 100,
+    marginTop: 20
+  }
 });
 
 export default HomeScreen;
