53de06b61feda05a37c6b9ce7c21c9068ef8fced
Neba Emmanuel <nebaemmanuel736@gmail.com>
2023-03-06 00:15:00 +0100

    colorCounter

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -1,11 +1,46 @@
 import React, {useState} from 'react';
 import {Text, View, StyleSheet, TouchableOpacity, FlatList} from 'react-native';
+import ColorCounter from './components/ColorCounter';
+
+const COLOR_INCREMENT = 15;
 
 const SquareScreen = () => {
+const [red, setRed] = useState(0);
+const [green, setGreen] = useState(0);
+const [blue, setBlue] = useState(0);
+
+const setColor = (color, change) => {
+    if(color === 'red'){
+        if(red + change > 255 || red + change < 0){
+            return;
+        }
+        else{
+            setRed(red + change);
+        }
+    }
+}
 
     return (
-        <View>          
-            <Text>SquareScreen</Text>
+        <View style={{marginHorizontal: 20}}>          
+            <ColorCounter 
+                onIncrease={() => {setColor('red',COLOR_INCREMENT)}}
+                onDecrease={() => {setColor('red',-1 * COLOR_INCREMENT)}}
+                color='Red'
+                amount={red}
+            />
+            <ColorCounter 
+                onIncrease={() => {setGreen(green + COLOR_INCREMENT)}}
+                onDecrease={() => {setGreen(green - COLOR_INCREMENT)}}
+                color='Green'
+                amount={green}
+            />
+            <ColorCounter 
+                onIncrease={() => {setBlue(blue + COLOR_INCREMENT)}}
+                onDecrease={() => {setBlue(blue - COLOR_INCREMENT)}}
+                color='Blue'
+                amount={blue}
+            />
+            <View style={{width: 100, height: 100, marginTop: 10, backgroundColor: `rgb(${red},${green},${blue})`}}></View>
         </View>
     );
 }
