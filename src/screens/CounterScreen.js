65cd35f91354e98f58383c25e9e855c660a9eb57
Neba Emmanuel <nebaemmanuel736@gmail.com>
2023-03-10 10:54:41 +0100

    counter with reducers

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,28 +1,39 @@
-import React, {useState} from 'react';
+import React, {useReducer} from 'react';
 import {Text, View, StyleSheet, TouchableOpacity} from 'react-native';
 
+//Community Conversion of using Reducer
+// a better name for action would be HowToChangeState
+const reducer = (state, action) => {
+    // state ==={ counter: number}
+    // action ==={type: Increment || Decrement, payload: 1}
+    switch(action.type){
+        case 'increment':
+            return {...state, counter: state.counter + action.payload};
+        case 'decrement':
+            return {...state, counter: state.counter - action.payload}
+        default:
+            return state;
+    } 
+}
 const CounterScreen = () => {
-const [counter, setCounter] = useState(0);
+    //a better name for dispatch will be CallMyReducer
+const [state, dispatch] = useReducer(reducer, {counter: 0});
 
     return (
         <View style={{flex: 1, alignItems: 'center'}}>
             <TouchableOpacity 
-            onPress={() => {
-                setCounter(counter + 1);
-            }}
+            onPress={() => dispatch({type: 'increment', payload: 1})}
             style={styles.buttonStyle}
             >
                 <Text>Increase</Text>
             </TouchableOpacity>
 
             <TouchableOpacity
-             onPress={() => {
-                setCounter(counter - 1);
-            }}
+             onPress={() => dispatch({type: 'decrement', payload: -1})}
             style={styles.buttonStyle}>
                 <Text>Decrease</Text>
             </TouchableOpacity>
-            <Text>Current Count: {counter}</Text>
+            <Text>Current Count: {state.counter}</Text>
         </View>
     );
 }
